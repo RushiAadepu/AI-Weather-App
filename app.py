5fa3fa49a8013ad1018ec1bdc2ace3b78950a5d9
RushiAadepu <123751805+RushiAadepu@users.noreply.github.com>
2023-10-08 13:21:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,9 +10,11 @@ WEATHER_API_URL = 'http://api.openweathermap.org/data/2.5/weather'
 # RapidAPI key for ChatGPT
 RAPIDAPI_KEY = '3965b429bfmsh160e49419126035p1d84efjsn4c96f585201e'  # Replace with your RapidAPI key
 
+# Include FontAwesome library
 st.markdown(
     f"""
     <style>
+    @import url('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css');
     body {{
         font-family: Arial, sans-serif;
     }}
@@ -93,6 +95,7 @@ def get_weather(location):
 
         return weather_info
 
+
 def predict_weather(features):
     # Reshape the 1D features list into a 2D array
     features_2d = [features]
@@ -100,6 +103,7 @@ def predict_weather(features):
     loaded_model = joblib.load('model.joblib')
     prediction = loaded_model.predict(features_2d)
     return prediction[0]
+
 
 def generate_response(prompt):
     url = "https://open-ai21.p.rapidapi.com/conversationgpt"
@@ -128,6 +132,7 @@ def generate_response(prompt):
     else:
         return f"Failed to get a response from the ChatGPT API. Status code: {response.status_code}"
 
+
 def main():
     st.title('AI Weather and Chatbot App')
 
@@ -138,13 +143,20 @@ def main():
 
         if weather_data is not None:
 
-            st.write(f"Temperature: {weather_data.get('temperature', 'N/A')}°C")
-            st.write(f"Humidity: {weather_data.get('humidity', 'N/A')}%")
-            st.write(f"Wind Speed: {weather_data['wind_speed']} km/h")
-            st.write(f"Pressure: {weather_data['pressure']} hPa")
-            st.write(f"Cloudiness: {weather_data['cloudiness']}%")
-            st.write(f"Sunrise Time: {weather_data['sunrise_time']}")
-            st.write(f"Sunset Time: {weather_data['sunset_time']}")
+            st.markdown(f'<i class="fas fa-thermometer-half"></i> Temperature: {weather_data.get("temperature", "N/A")}°C',
+                        unsafe_allow_html=True)
+            st.markdown(f'<i class="fas fa-tint"></i> Humidity: {weather_data.get("humidity", "N/A")}%',
+                        unsafe_allow_html=True)
+            st.markdown(f'<i class="fas fa-wind"></i> Wind Speed: {weather_data["wind_speed"]} km/h',
+                        unsafe_allow_html=True)
+            st.markdown(f'<i class="fas fa-compress-arrows-alt"></i> Pressure: {weather_data["pressure"]} hPa',
+                        unsafe_allow_html=True)
+            st.markdown(f'<i class="fas fa-cloud"></i> Cloudiness: {weather_data["cloudiness"]}%',
+                        unsafe_allow_html=True)
+            st.markdown(f"<i class='fas fa-sun'></i> Sunrise Time: {weather_data['sunrise_time']}",
+                        unsafe_allow_html=True)
+            st.markdown(f"<i class='fas fa-moon'></i> Sunset Time: {weather_data['sunset_time']}",
+                        unsafe_allow_html=True)
 
             image_path = 'weather-images.jpg'
             st.image(image_path, caption='Types of Weather', use_column_width=True)
@@ -158,31 +170,33 @@ def main():
             ]
             predicted_condition = predict_weather(features)
 
-            st.markdown(f"<h2>Today's Predicted Weather Condition: {predicted_condition}</h2>", unsafe_allow_html=True)
             if predicted_condition == 'normal':
                 st.markdown(
-                    "<p style='font-size: 18px;'>It seems like a normal day. You can go about your usual activities.</p>",
+                    "<p style='font-size: 18px;'><i class='fas fa-cloud-sun'></i> It seems like a normal day. You can go about your usual activities.</p>",
                     unsafe_allow_html=True)
             elif predicted_condition == 'cloudy':
                 st.markdown(
-                    "<p style='font-size: 18px;'>The sky is cloudy today. It might be a good idea to carry an umbrella.</p>",
+                    "<p style='font-size: 18px;'><i class='fas fa-cloud'></i> The sky is cloudy today. It might be a good idea to carry an umbrella.</p>",
                     unsafe_allow_html=True)
             elif predicted_condition == 'sunny':
                 st.markdown(
-                    "<p style='font-size: 18px;'>It's a sunny day! Don't forget to wear sunscreen and stay hydrated.</p>",
+                    "<p style='font-size: 18px;'><i class='fas fa-sun'></i> It's a sunny day! Don't forget to wear sunscreen and stay hydrated.</p>",
                     unsafe_allow_html=True)
             elif predicted_condition == 'partly cloudy':
                 st.markdown(
-                    "<p style='font-size: 18px;'>The weather is partly cloudy. Enjoy your day with a mix of sun and clouds.</p>",
+                    "<p style='font-size: 18px;'><i class='fas fa-cloud-sun'></i> The weather is partly cloudy. Enjoy your day with a mix of sun and clouds.</p>",
                     unsafe_allow_html=True)
             elif predicted_condition == 'hot':
-                st.markdown("<p style='font-size: 18px;'>It's going to be a hot day. Stay cool and stay hydrated.</p>",
-                            unsafe_allow_html=True)
+                st.markdown(
+                    "<p style='font-size: 18px;'><i class='fas fa-thermometer-full'></i> It's going to be a hot day. Stay cool and stay hydrated.</p>",
+                    unsafe_allow_html=True)
             elif predicted_condition == 'rainy':
-                st.markdown("<p style='font-size: 18px;'>Expect rain today. Don't forget your umbrella!</p>",
-                            unsafe_allow_html=True)
+                st.markdown(
+                    "<p style='font-size: 18px;'><i class='fas fa-cloud-showers-heavy'></i> Expect rain today. Don't forget your umbrella!</p>",
+                    unsafe_allow_html=True)
             else:
                 st.write("The weather condition is uncertain. Stay prepared for any changes.")
+
         else:
             st.warning(
                 "Oops! The weather information for the provided location is not available. Please make sure the location is valid and try again.")
@@ -197,6 +211,7 @@ def main():
             response = generate_response(user_input)
             assistant_reply = response.get('GPT', 'N/A')  # Extract GPT response content
             st.write(f"AI: {assistant_reply}")
+
 
 if __name__ == '__main__':
     main()
